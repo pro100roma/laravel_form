9fdc57d4f0ba462ebe26f34eda8eb78f854b0fa4
pro100roma <roman.vorobiov94@gmail.com>
2019-12-19 20:29:19 +0200

    24hours form

--- app/Http/Controllers/requestController.php
+++ app/Http/Controllers/requestController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use DB, Requests, Auth;
+use DB, Requests, Auth, Mail;
 
 class requestController extends Controller
 {
@@ -15,12 +15,22 @@ public function formSubmit(Request $request) {
     //     'body' => $request->message
     // ]);
 
+    //Insert data to DB
     $req = new \App\Requests;
     $req->subject = $request->input('subject');
     $req->body = $request->input('message');
     $req->client_name = Auth::user()->name;
     $req->client_email = Auth::user()->email;
     $req->save();
+
+    //Send email to admin
+    // $to_name =  Auth::user()->name;
+    // $to_email = 'chronos945@mail.ru';//Auth::user()->email;
+    // $data = array('name'=>Auth::user()->name, "body" => "Test email");
+    // Mail::send(['text' => 'mail'], $data, function($message) use ($to_name, $to_email) {
+    //     $message->to($to_email, $to_name)->subject('Web Testing Mail');
+    //     $message->from('chronos945@gmail.com', 'Artisans Web');
+    // });
 
     //return Redirect::back();
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,25 +14,44 @@
                         </div>
                     @endif
 
-                    You are logged in, <strong>{{ Auth::user()->name }}</strong>
+                    <p>You are logged in, <strong>{{ Auth::user()->name }}</strong></p>
+                    <p>Email: <strong>{{ Auth::user()->email }}</strong></p>
                     {{-- <p><a href="/send">Send me email</a></p> --}}
+                    
+                    <?php use Carbon\Carbon; 
+                    $requestEmail = App\Requests::where('client_email', Auth::user()->email)->get();
+                    $lastRequest = App\Requests::where('client_email', Auth::user()->email)
+                        ->orderBy('created_at', 'desc')
+                        ->pluck('created_at')
+                        ->first();
+                        
+                    $date = Carbon::now();
+                    $diff = $date->diffInHours($lastRequest); ?>
+
                     @if ( Auth::user()->admin == 1 )
                         <p>You are admin</p>
                         <a href="/requests" class="btn btn-primary">Requests</a>
                     @endif
                     @if ( Auth::user()->admin == 0 )
-                        <form action="/requests/new_request" method="POST">
-                            {{csrf_field()}}
-                            <div class="input-group">
-                                <input type="text" name="subject" class="form-control" placeholder="Subject" required>
-                            </div>
-                            <div class="input-group">
-                                <textarea class="form-control" name="message" placeholder="Message"></textarea>
-                            </div>
-                            <div class="input-group input-group-btn">
-                                <input type="submit" class="btn btn-primary" value="Send">
-                            </div>
-                        </form>
+                        <p>Количество запросов: {{count($requestEmail)}}</p>
+                        @if (count($requestEmail) == 0 || $diff >= 24)
+                            <form action="/requests/new_request" method="POST">
+                                {{csrf_field()}}
+                                <div class="input-group">
+                                    <input type="text" name="subject" class="form-control" placeholder="Subject" required>
+                                </div>
+                                <div class="input-group">
+                                    <textarea class="form-control" name="message" placeholder="Message"></textarea>
+                                </div>
+                                <div class="input-group input-group-btn">
+                                    <input type="submit" class="btn btn-primary" value="Send">
+                                </div>
+                            </form>
+                        @else
+                            <p>Last request: <strong>{{$diff}}</strong> hours ago.</p>
+                            <p>You can make your next request in {{24 - $diff}} hours</p>
+                        @endif
+
                     @endif
                 </div>
             </div>
